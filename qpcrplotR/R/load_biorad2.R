cb5dd7653b85c00252470702903a7971590874a4
Matt Tuttle <mattjtuttle@gmail.com>
2017-04-21 13:58:53 -0400

    Updated load_biorad2 function to calculate basic stats

--- qpcrplotR/R/load_biorad2.R
+++ qpcrplotR/R/load_biorad2.R
@@ -8,15 +8,26 @@
 #' load_biorad2("my_data.csv")
 #' load_biorad2("./data/my_data.csv")
 #' @import tidyverse
+#' @import tibble
 #' @export
 
-# Imports the file as a tibble so that package tools can easily be used on it
+# Imports the file as a tibble and calculates means so that package tools can easily be used on it
 load_biorad2 <- function(datafile) {
+
+  #Imports data from the .csv file
   headers <- read.csv(datafile, header = FALSE, nrows = 1, as.is = TRUE)
   raw_data <- read.csv(datafile, skip = 3, header = FALSE) %>%
     as_tibble()
   colnames(raw_data) <- headers
+
+  #Selects important data from the imported .csv file and calculates basic statistics by sample
   selected_data <- filter(raw_data, Content == "Sample") %>%
-    select(Sample = Description, Copies = copies)
+    select(Sample = Description, Copies = copies) %>%
+    group_by(Sample) %>%
+    mutate(Mean = mean(Copies)) %>%
+    mutate(Standard.deviation = sd(Copies)) %>%
+    ungroup() %>%
+    arrange(Sample)
+
   return(selected_data)
 }
